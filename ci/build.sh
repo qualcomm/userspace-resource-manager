23f504e0faa737028618dc67a96e2747cd662b4e
kartnema <kartnema@qti.qualcomm.com>
2026-02-16 20:03:43 +0530

    license fix

--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
 mkdir -p build
 cd build
 cmake .. -DBUILD_TESTS=ON -DBUILD_CLASSIFIER=ON -DCMAKE_INSTALL_PREFIX=/
